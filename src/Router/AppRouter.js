a8dde650e244e1dfb8313db59e1511dc5ed617d1
BastinJafari <BastinJafari@gmail.com>
2020-04-22 16:09:10 +0200

    Change css of secret page

--- src/Router/AppRouter.js
+++ src/Router/AppRouter.js
@@ -2,11 +2,13 @@ import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom'
 import React from 'react'
 import PostSecretPage from '../components/PostSecretPage'
 import ForwardOrMessage from '../components/ForwardOrMessage';
-import ReadSecretPageStaging from '../components/ReadSecretPageStaging';
+import ReadSecretPage from '../components/ReadSecretPage';
 const AppRouter = () => {
     return (
         <BrowserRouter>
             <Switch>
+            <Route path="/:staging" component={ReadSecretPage}/>
+
                 <Route path="/:secretKey" component={ForwardOrMessage}/>
                 <Route path="/" component={PostSecretPage} exact={true} />
             </Switch>
