0653778b0d1ff538520a0872365800486c5870d9
BastinJafari <BastinJafari@gmail.com>
2020-04-22 09:25:55 +0200

    Fix error message bug

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -2,12 +2,11 @@ import isURL from 'validator/lib/isURL';
 
 export default (values) => {
     const errors = {}
-
     if (!values.url) {
         errors.url = 'Required'
     } else if (!isURL(values.url)) {
         errors.url = 'Invalid url '
-    } else if (values.message.length > 1000) {
+    } else if (values.message?.length > 1000) {
         errors.message = 'Message too long '
     }
     return errors
