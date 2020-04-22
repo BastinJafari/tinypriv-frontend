8b1037e4b718a545e48504a4c33576648cf70daa
BastinJafari <BastinJafari@gmail.com>
2020-04-22 15:13:24 +0200

    remove console logs

--- src/utils/requestHandlers/getSecret.js
+++ src/utils/requestHandlers/getSecret.js
@@ -1,14 +1,11 @@
 const serverAdress = process.env.REACT_APP_GET_SECRET_API + "/getSecret?key="
 
 export default (key, onGetSecret) => {
-    console.log("KEY THAT GOT TO GET SECRET: " + key)
     let xhr = new XMLHttpRequest()
-    console.log(serverAdress + `${key}`)
     xhr.open("GET", serverAdress + `${key}`)
     
     xhr.onload = () => {
         const secret = JSON.parse(xhr.response)
-        console.log("response: " + secret.url)
         onGetSecret(secret)
     }
 
--- src/utils/requestHandlers/postSecret.js
+++ src/utils/requestHandlers/postSecret.js
@@ -1,14 +1,12 @@
 const serverAdress = process.env.REACT_APP_POST_SECRET_API
 
 export default (secret, onSecretPosted) => {
-    console.log(serverAdress)
     let xhr = new XMLHttpRequest()
     xhr.open("POST", serverAdress, true)
     xhr.setRequestHeader("Content-Type", "multipart/form-data")
 
     xhr.onreadystatechange = function() { 
         if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-            console.log(xhr.response)
             const shortUrl = JSON.parse(xhr.response).shortUrl
             onSecretPosted(shortUrl)
         }
