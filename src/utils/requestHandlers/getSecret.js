751aadbd7b9c8f0275b4fb7f202f5259c3e2caba
BastinJafari <BastinJafari@gmail.com>
2020-04-24 15:20:00 +0200

    refactor

--- src/utils/requestHandlers/getSecret.js
+++ src/utils/requestHandlers/getSecret.js
@@ -1,13 +1,40 @@
-const serverAdress = process.env.REACT_APP_GET_SECRET_API + "/getSecret?key="
+const serverAdress = process.env.REACT_APP_GET_SECRET_API
+
+const sendReadReceipt = (key) => {
+    let xhr = new XMLHttpRequest()
+
+    try {
+        xhr.open("DETE", `${serverAdress}/`, true)
+        xhr.setRequestHeader("Content-Type", "multipart/form-data")
+
+        xhr.onreadystatechange = function () {
+            if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+                return JSON.parse(xhr.response).deletionSuccess
+            }
+        }
+
+        xhr.send(JSON.stringify({key}))
+    } catch (error) {
+        return error
+    }
+
+}
 
 export default (key, onGetSecret) => {
     let xhr = new XMLHttpRequest()
-    xhr.open("GET", serverAdress + `${key}`)
     
-    xhr.onload = () => {
-        const secret = JSON.parse(xhr.response)
-        onGetSecret(secret)
-    }
+    try {
+        xhr.open("GET", `${serverAdress}/?key=${key}`)
 
-    xhr.send()
+        xhr.onload = () => {
+            const secret = JSON.parse(xhr.response)
+            onGetSecret(secret)
+            sendReadReceipt(key)
+        }
+    
+        xhr.send()
+
+    } catch (error) {
+        return error
+    }
 }
